f482d54e778586b44dcda1d5650aac5c4391fe62
whats2000 <60466660+whats2000@users.noreply.github.com>
2025-02-01 17:24:30 +0800

    Update answer.ts

--- 3151-Special Array I/answer.ts
+++ 3151-Special Array I/answer.ts
@@ -1,15 +1,13 @@
 function isArraySpecial(nums: number[]): boolean {
-  // let previousNumberIsOdd = nums[0] % 2 === 1;
-  //
-  // for (let i = 1; i < nums.length; i++) {
-  //   const currentNumberIsOdd = nums[i] % 2 === 1;
-  //   if (previousNumberIsOdd === currentNumberIsOdd) {
-  //     return false;
-  //   }
-  //   previousNumberIsOdd = currentNumberIsOdd;
-  // }
-  //
-  // return true;
+  let previousNumberIsOdd = nums[0] % 2 === 1;
 
-  return nums.every((num, i) => i === 0 || (nums[i - 1] % 2 !== num % 2));
+  for (let i = 1; i < nums.length; i++) {
+    const currentNumberIsOdd = nums[i] % 2 === 1;
+    if (previousNumberIsOdd === currentNumberIsOdd) {
+      return false;
+    }
+    previousNumberIsOdd = currentNumberIsOdd;
+  }
+
+  return true;
 }
